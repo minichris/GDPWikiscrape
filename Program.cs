4274a5fd8550216fc6ab798e3613331bc397dc5d
Christopher Lee <minichris502@gmail.com>
2018-10-05 23:19:30 +0100

    We nolonger rebuild the entire games.json file if it already exists

--- Program.cs
+++ Program.cs
@@ -23,18 +23,18 @@ namespace Parser
             var gamesWithCategories = GetGamesWithCategories();
             GameNames = gamesWithCategories.Keys.ToList();
             GameCategories = gamesWithCategories.Values.SelectMany(x => x).ToList();
-            File.WriteAllText("games.json", JsonConvert.SerializeObject(gamesWithCategories));
+            
 
             List<Pattern> Patterns = new List<Pattern>();
             foreach (var pattern in patternsScraper.PagesDictionary)
             {
                 Pattern patternOutput;
-                if (!File.Exists(Pattern.GetFileName(pattern.Key))){
+                if (!File.Exists(Pattern.GetFileName(pattern.Key))){ //if we don't have this pattern file yet
                     PatternPageScraper patternScraper = new PatternPageScraper(pattern.Value);
                     patternScraper.Start();
                     patternOutput = patternScraper.patternObject;
                 }
-                else
+                else //if we already have the pattern file
                 {
                     string text = File.ReadAllText(Pattern.GetFileName(pattern.Key));
                     patternOutput = JsonConvert.DeserializeObject<Pattern>(text);
@@ -60,7 +60,7 @@ namespace Parser
                     if (patternObject.PatternsLinks.Any(x => x.To == "Movement")) {
                         foreach (Pattern.PatternLink link in patternObject.PatternsLinks)
                         {
-                            if (link.Type == Pattern.PatternLink.LinkType.Pattern)
+                            if (link.Type == Pattern.PatternLink.LinkType.Pattern) //enforces that we only link to other patterns
                             {
                                 links.Add(new
                                 {
@@ -79,32 +79,43 @@ namespace Parser
 
         static Dictionary<String, List<String>> GetGamesWithCategories() //this fuction gets a kvp of Games with there Subcategory
         {
-            //Scrape the games category
-            CategoryScraper gamesScraper = new CategoryScraper("Games");
-            gamesScraper.Start();
-
             //Create a dictionary to hold games (key) with its subcategories (value)
             Dictionary<String, List<String>> GamesWithCategories = new Dictionary<String, List<String>>();
 
-            //iterate though all the subcategories
-            foreach (string gameSubcategoryName in gamesScraper.SubcategoryDictionary.Keys.ToList())
+            if (!File.Exists("games.json")) //if the file doesn't exist already
             {
-                CategoryScraper gamesSubcategoryScraper = new CategoryScraper(gameSubcategoryName);
-                gamesSubcategoryScraper.Start();
+                Console.WriteLine("Getting games with their categories: Fresh.");
 
-                //iterate though all the games in that subcategory
-                foreach(string gameTitle in gamesSubcategoryScraper.PagesDictionary.Keys)
+                //Scrape the games category
+                CategoryScraper gamesScraper = new CategoryScraper("Games");
+                gamesScraper.Start();
+
+                //iterate though all the subcategories
+                foreach (string gameSubcategoryName in gamesScraper.SubcategoryDictionary.Keys.ToList())
                 {
-                    if (GamesWithCategories.ContainsKey(gameTitle)) //if we already have the game in the dictionary
+                    CategoryScraper gamesSubcategoryScraper = new CategoryScraper(gameSubcategoryName);
+                    gamesSubcategoryScraper.Start();
+
+                    //iterate though all the games in that subcategory
+                    foreach (string gameTitle in gamesSubcategoryScraper.PagesDictionary.Keys)
                     {
-                        GamesWithCategories.First(x => x.Key == gameTitle).Value.Add(gameSubcategoryName);
+                        if (GamesWithCategories.ContainsKey(gameTitle)) //if we already have the game in the dictionary
+                        {
+                            GamesWithCategories.First(x => x.Key == gameTitle).Value.Add(gameSubcategoryName);
+                        }
+                        else //if it doesn't exist in the dictionary already
+                        {
+                            GamesWithCategories.Add(gameTitle, new List<String>() { gameSubcategoryName });
+                        }
+
                     }
-                    else //if it doesn't exist in the dictionary already
-                    {
-                        GamesWithCategories.Add(gameTitle, new List<String>() { gameSubcategoryName });
-                    }
-                    
                 }
+                File.WriteAllText("games.json", JsonConvert.SerializeObject(GamesWithCategories));
+            }
+            else //if the file does exist
+            {
+                Console.WriteLine("Getting games with their categories: from existing file.");
+                GamesWithCategories = JsonConvert.DeserializeObject<Dictionary<String, List<String>>>(File.ReadAllText("games.json"));
             }
             return GamesWithCategories;
         }
